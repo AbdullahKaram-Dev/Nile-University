00280a52539568a079ade6e2314f0dafb8345e19
AbdullahKaram-Dev <abdallakaramdev@gmail.com>
2021-12-07 17:59:14 +0200

    second

--- app/Http/Controllers/Web/Admin/AdminUserController.php
+++ app/Http/Controllers/Web/Admin/AdminUserController.php
@@ -14,6 +14,7 @@ class AdminUserController extends Controller
 
     public function __construct(AdminUserInterface $AdminUserInterface)
     {
+        /* test github */
         $this->AdminUserInterface = $AdminUserInterface;
     }
 
